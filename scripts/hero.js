3cc8108c74eb01e76d8f99572640e2e92c507a12
Angela <angdkay@gmail.com>
2025-08-28 17:54:32 -0600

    Integrate odin logo and downward arrow animation

---
+++ scripts/hero.js
@@ -0,0 +1,31 @@
+/*
+  Code below makes the odin project logo appear when hover over "Odin Project" link
+*/
+
+const odinLogo = document.querySelector(".odin-link-img");
+const odinLink = document.querySelector(".odin-link");
+let timer;
+let locked = false;
+
+// Delay listening into hover to wait for assets to load and animate in fully
+setTimeout(() => {
+  odinLink.addEventListener("mouseenter", () => {
+    if (locked) return;
+    locked = true;
+
+    odinLogo.style.visibility = "visible";
+    odinLogo.style.animation = "none";
+    void odinLogo.offsetWidth;
+    odinLogo.style.animation = "reveal-odin 1.2s ease-in-out forwards";
+
+    timer = setTimeout(() => {
+      odinLogo.style.animation = "none";
+      void odinLogo.offsetWidth;
+      odinLogo.style.animation = "hide-odin 1.2s ease-in-out reverse forwards";
+
+      setTimeout(() => {
+        locked = false;
+      }, 1200);
+    }, 5000)
+});
+}, 3000);
