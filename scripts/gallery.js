81e2499ce0cce2dd46f700671035e290b41e7747
Angela <angdkay@gmail.com>
2025-08-30 23:06:14 -0600

    Revise font sizes and file names

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -9,31 +9,14 @@ const swiper = new Swiper(".mySwiper", {
   speed: 800,
   on: {
     slideChangeTransitionStart: function () {
-      // remove old direction classes
-      this.slides.forEach(slide => {
-        slide.classList.remove("from-left", "from-right");
-      });
-
-      // get the new active slide
-      const active = this.slides[this.activeIndex];
-
-      // force reflow so animation can re-trigger
-      void active.offsetWidth;
-
-      // add class depending on swipe direction
-      if (this.swipeDirection === "next") {
-        active.classList.add("from-right");
-      } else if (this.swipeDirection === "prev") {
-        active.classList.add("from-left");
-      }
-
       // reset all frames to silver
       document.querySelectorAll(".swiper-slide .frame").forEach(frame => {
         frame.src = "assets/bg-imgs/frame-silver.png";
       });
 
       // set active frame to gold
-      const activeFrame = active.querySelector(".frame");
+      const activeSlide = this.slides[this.activeIndex];
+      const activeFrame = activeSlide.querySelector(".frame");
       if (activeFrame) {
         activeFrame.src = "assets/bg-imgs/frame-gold.png";
       }
