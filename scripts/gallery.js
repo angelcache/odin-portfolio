e727102f870b9cfa1e7b4cc1b34921dc2b44a1e3
Angela <angdkay@gmail.com>
2025-09-01 14:05:48 -0600

    Make gallery responsive

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,12 +1,23 @@
 const swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,        
-  centeredSlides: true,                
-  spaceBetween: -800,       
+  slidesPerView: 1,
+  centeredSlides: true,
+  spaceBetween: 100,
+  speed: 800,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
   speed: 800,
+  breakpoints: {
+    600: {
+      slidesPerView: 1,
+      spaceBetween: -200,
+    },
+    1025: {
+      slidesPerView: 3,
+      spaceBetween: -750
+    }
+  },
   on: {
     slideChangeTransitionStart: function () {
       // reset all frames to silver
@@ -24,8 +35,4 @@ const swiper = new Swiper(".mySwiper", {
   }
 });
 
-// initialize frames at load
-document.querySelectorAll(".swiper-slide .frame").forEach(frame => {
-  frame.src = "assets/bg-imgs/frame-silver.png";
-});
 swiper.slides[swiper.activeIndex].querySelector(".frame").src = "assets/bg-imgs/frame-gold.png";
